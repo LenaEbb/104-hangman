d9e355dabc256674d10e483fa67edbf21384bbec
Lena Ebb <lenafreediving@gmail.com>
2018-10-16 10:04:16 +0200

    first bug fix

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -19,25 +19,34 @@ module Hangman
         char = gets.chomp
         Graphics.clear_screen
 
+
         if word.include? char
+
+          if guess.nil? || guess == ""
+            puts 'Seriously? You gotta type a letter!'
+            puts 'Try again: ' + Graphics.obfuscate_word(word, guess)
+          end
+
           if guess.include? char
             puts "You already entered '#{char}'. Yes, it is still correct.. 🙄"
             puts 'Try again: ' + Graphics.obfuscate_word(word, guess)
           else
             guess << char
             placeholder = Graphics.obfuscate_word(word, guess)
-
             puts 'Whoop Whoop!! ' + placeholder
           end
 
-          unless placeholder.include? Graphics::OBFUSCATION_CHAR
+            unless placeholder.include? Graphics::OBFUSCATION_CHAR
             puts Graphics::ALIVE
             puts "\n\nWELL DONE!! YOU SURVIVED"
             break
           end
+
         else
           puts "OH NOES! The word doesn't contain '#{char}'"
           @wrong_tries = @wrong_tries + 1
+
+
 
           if wrong_tries == chances
             puts Graphics::DEAD
